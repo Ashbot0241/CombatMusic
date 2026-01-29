4cd432a69c3af6ddd7e5978133a4f5404b7ef072
Greiz <130429621+Ashbot0241@users.noreply.github.com>
2026-01-29 18:21:34 +1100

    Minor updates and testing

--- src/init.lua
+++ src/init.lua
@@ -31,7 +31,7 @@ local date = date
 local AddOn = LibStub("AceAddon-3.0"):NewAddon(AddOnName, "AceEvent-3.0", "AceTimer-3.0", "LibVan32-2.0")
 --LibStub("LibVan32-1.0"):Embed(AddOn, canonicalTitle)
 
-AddOn._major = "6.0.1"
+AddOn._major = "6.0.2"
 AddOn._revision = "91746d8"
 
 
